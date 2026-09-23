10b73d6cc768aac211fd7b0b02f5aee582bf62c6
zttjm <3353373185@qq.com>
2026-09-23 13:37:49 +0800

    task7脚本

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -4,7 +4,20 @@
 # Usage: ./scripts/analyze.sh FILE
 
 # TODO: validate arguments
+if [[ $# -eq 0 ]]; then
+    echo "Usage: ./scripts/analyze.sh FILE"
+    exit 1
+fi
 # TODO: validate file existence
+if [[ ! -f "$1" ]]; then
+    echo "Error: File '$1' not found or is not a regular file."
+    exit 1
+fi
 # TODO: print:
 # Total ERROR: <number>
+error_count=$(grep -c "ERROR" "$1")
+echo "Total ERROR: ${error_count}"
 # Top Code: <code>
+top_code=$(cut -d ' ' -f 5 "$1" | cut -d '=' -f 2 | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
+echo "Top Code: ${top_code}"
+exit 0
