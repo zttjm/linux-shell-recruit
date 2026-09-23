54d9f945c9b786c694c2e753c3ab74a90aad5070
zttjm <3353373185@qq.com>
2026-09-23 13:39:54 +0800

    task 08 脚本

--- scripts/batch-copy.sh
+++ scripts/batch-copy.sh
@@ -3,12 +3,12 @@
 # Task 08: this script is intentionally buggy.
 # Usage: ./scripts/batch-copy.sh DEST FILE...
 
-destination=$1
+destination="$1"
 shift
 
 mkdir -p $destination
 
-for file in $@
+for file in "$@"
 do
-    cp $file $destination/
+    cp "$file" $destination/
 done
